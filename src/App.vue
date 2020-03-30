723c3cf79d4158bc2128bf8725f0d620f8d594b2
yuanaodi <1925349682@qq.com>
2020-03-30 14:46:36 +0800

    页面布局结构

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="container">
+    <div class="box">
+      <header class="header">头部</header>
+      <div class="content">
+      </div>
     </div>
-    <router-view/>
+    <footer class="footer">底部</footer>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import '@/lib/reset.scss'; // 引入重置样式, @表示src目录
+html, body, .container {
+  @include rect(100%, 100%)
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+.container {
+  // 使用弹性盒布局
+  @include flexbox();
+  @include flex-direction(column);
+  .box {
+    @include flex(); // flex为1
+    @include rect(100%, auto);
+    @include flexbox();
+    @include flex-direction(column);
+    .header {
+      @include rect(100%, 0.44rem);
+      @include background-color(#f66);
     }
+    .content {
+      @include flex();
+      @include rect(100%, auto);
+      @include overflow(); // 超出滑动
+    }
+  }
+  .footer {
+    @include rect(100%, 0.5rem);
+    @include background-color(#efefef)
   }
 }
 </style>
